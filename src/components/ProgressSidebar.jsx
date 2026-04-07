60846918560eb5b13fad16eaf690547ba145335e
Dugiii <dugidemeg@gmail.com>
2026-04-06 23:59:17 -0500

    created progress sidebar component

--- src/components/ProgressSidebar.jsx
+++ src/components/ProgressSidebar.jsx
@@ -1,37 +1,53 @@
+import { useStepProgress } from "../store/stepProgress"
+
 export const ProgressSidebar = () => {
+    const currentStep = useStepProgress((state) => state.currentStep);
+
     {/* <!-- * Progress Section --> */}
     return (
         <section className="progress-section">
-            <div className="progress-section-mobile"> {/* <!-- * Progress Section Mobile -->*/}
+            <div className="progress-section-mobile"> {/* <!-- * Mobile Container-->*/}
 
-                <div className="progress-circle">
-                    <span className="step-number active">1</span>
+                {[1,2,3,4].map(num => (
+                    <div key={num} className="progress-circle">
+                    <span className={`step-number ${currentStep >= num ? 'active' : ''}`}>{num}</span>
+                    <div className="user-info-step">
+                        <span className="step">{`Step ${num}`}</span>
+                        <span className="step-name">{
+                        ['Your Info','Select Plan','Add-ons','Summary'][num-1]
+                        }</span>
+                    </div>
+                    </div>
+                ))}
+
+                {/* <div className="progress-circle">
+                    <span className={`step-number ${currentStep >= 1 ? 'active' : ''}`}>1</span>
                     <div className="user-info-step">
                         <span className="step">Step 1</span>
                         <span className="step-name">Your Info</span>
                     </div>
                 </div>
                 <div className="progress-circle">
-                    <span className="step-number">2</span>
+                    <span className={`step-number ${currentStep >= 2 ? 'active' : ''}`}>2</span>
                     <div className="user-info-step">
                         <span className="step">Step 2</span>
                         <span className="step-name">Select Plan</span>
                     </div>
                 </div>
                 <div className="progress-circle">
-                    <span className="step-number">3</span>
+                    <span className={`step-number ${currentStep >= 3 ? 'active' : ''}`}>3</span>
                     <div className="user-info-step">
                         <span className="step">Step 3</span>
                         <span className="step-name">Add-ons</span>
                     </div>
                 </div>
                 <div className="progress-circle">
-                    <span className="step-number">4</span>
+                    <span className={`step-number ${currentStep >= 4 ? 'active' : ''}`}>4</span>
                     <div className="user-info-step">
                         <span className="step">Step 4</span>
                         <span className="step-name">Summary</span>
                     </div>
-                </div>
+                </div> */}
 
             </div>
         </section>
