b4ff3bcdf39ec8d17d41cdb1e6375a154829993c
Dugiii <dugidemeg@gmail.com>
2026-04-07 01:09:23 -0500

    created thank you section component

---
+++ src/components/ThankYou.jsx
@@ -0,0 +1,8 @@
+export const ThankYou = () => {
+    {/* <!-- * Step 5: Thank You Section --> */}
+    return (
+        <section className="form form-mobile thank-you-section hidden">
+
+        </section>
+    )
+}
