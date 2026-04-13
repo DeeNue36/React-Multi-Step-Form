fa240f9f9cb16c69f7554e7e71f4736d9bd2c659
Dugiii <dugidemeg@gmail.com>
2026-04-12 23:34:30 -0500

    feat: implement logic for adding and removing selected addons in zustand store

--- src/store/addonsSelection.jsx
+++ src/store/addonsSelection.jsx
@@ -2,14 +2,26 @@ import { create } from 'zustand';
 
 export const useAddonsSelection = create((set) => ({
     selectedAddons: [],
-    setAddons: (addon) => {
-        set((state) => ({
-            selectedAddons: [...state.selectedAddons, addon]
-        }))
-    },
 
-    // Todo: implement adding active class for selected addons
-    // setSelectedAddons: (addon) => {
-    //     set((state) => {})
-    // }
+    setAddons: (addonId) => {
+        set((state) => {
+            const addonsCurrentlySelected = state.selectedAddons;
+            const isSelected = addonsCurrentlySelected.includes(addonId);
+
+            let newAddons; // store the list of selected addons
+
+            if(isSelected) {
+                // if the addon has been selected remove it from the list
+                newAddons = addonsCurrentlySelected.filter((id) => id !== addonId);
+            }
+            else {
+                // else add the addon to the list
+                newAddons = [...addonsCurrentlySelected, addonId]
+            }
+
+            return {
+                selectedAddons: newAddons
+            };
+        });
+    },
 }))
